9628291886cc812f43019bfae248717f92b9f1f0
MutatedGamer <lukebordonaro@gmail.com>
2018-09-12 11:46:37 -0400

    added eb packages

---
+++ main/management/commands/createsu.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from users.models import CustomUser
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not CustomUser.objects.filter(username="lukebord").exists():
+            User.objects.create_superuser("lukebord", "lukebord@mit.edu", "admin")
